1619adcff7201e6191d649f88308647d5979e84e
Bharathdev07 <devops854@gmail.com>
2024-09-04 16:39:03 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,41 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "enable_nat_gateway" {
+  description = "Whether to create NAT Gateways"
+  type        = bool
+  default     = true
+}
